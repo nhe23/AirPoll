3790c262888ea1326c66c05b36819f2ea294edc3
Nathan Herrmann <nathan.herrmann@hotmail.de>
2021-01-04 20:49:02 +0100

    fixed test

--- Frontend/src/components/Measurements/MeasurementsDashboard.spec.ts
+++ Frontend/src/components/Measurements/MeasurementsDashboard.spec.ts
@@ -1,15 +1,40 @@
 import fetchMock from "jest-fetch-mock";
 import MeasurementsDashboard from "./MeasurementsDashboard.svelte";
+import { mocked } from "ts-jest/utils";
+import { query } from "svelte-apollo";
 import { render } from "@testing-library/svelte";
+import { sleep } from "../../helpers/sleep";
+import { measurements } from "./mock/measurements";
 
 beforeAll(() => {
   fetchMock.enableMocks();
 });
+let mockedQuery: any;
+jest.mock("svelte-apollo");
+mockedQuery = mocked(query, true);
+
+mockedQuery.mockReturnValue({
+  // @ts-ignore
+  result: () => {
+    return measurements;
+  },
+});
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 it("should render measurement components", async () => {
   const { getByTestId } = render(MeasurementsDashboard);
   const desktop = getByTestId("measurementsDashboard");
-  
-  expect(desktop.querySelector('div[data-testid="measurementsContainer"]')).not.toBe(null);
-  expect(desktop.querySelector('div[data-testid="filterDesktop"]')).not.toBe(null);
-  expect(desktop.querySelector('div[data-testid="measurementsDescription"]')).not.toBe(null);
+  await sleep(1000);
+
+  expect(
+    desktop.querySelector('div[data-testid="measurementsContainer"]')
+  ).not.toBe(null);
+  expect(desktop.querySelector('div[data-testid="filterDesktop"]')).not.toBe(
+    null
+  );
+  expect(
+    desktop.querySelector('div[data-testid="measurementsDescription"]')
+  ).not.toBe(null);
 });
